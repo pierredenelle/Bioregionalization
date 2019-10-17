283fd4ff1e051498502fa701a8cd980c6886fa71
pierredenelle <pierre.denelle@gmail.com>
2019-10-17 16:56:34 +0200

    run_oslom fixes

--- R/run_oslom.R
+++ R/run_oslom.R
@@ -43,10 +43,22 @@ run_oslom <- function(dat, n_runs = 10, t_param = 0.1, cp_param = 0.5,
          the bioregions identified will be saved.")
   }
 
+  # Move OSLOM to saving_directory
+  # path1 <- paste0(.libPaths(), "/Bioregionalization/OSLOM/")
+  # current_files <- list.files(path1, full.names = TRUE)
+  # path2 <- saving_directory
+  # file.copy(from = current_files, to = path2, recursive = TRUE,
+  #           copy.mode = TRUE)
+
   # Save input dataset as a .txt file into OSLOM folder
   write.table(dat,
+              # paste0(saving_directory, "dataset.txt"),
               paste0(.libPaths(), "/Bioregionalization/OSLOM/dataset.txt"),
               row.names = FALSE)
+
+  # Change working directory so the file is saved in the proper place
+  current_path <- getwd()
+  setwd(paste0(.libPaths(), "/Bioregionalization/"))
 
   # Set up the command with required parameters
   if(.Platform$OS.type == "windows"){
@@ -57,6 +69,12 @@ run_oslom <- function(dat, n_runs = 10, t_param = 0.1, cp_param = 0.5,
       paste0(.libPaths(),
              "/Bioregionalization/OSLOM/oslom_undir_win.exe -f OSLOM/dataset.txt -w",
              " -r ", n_runs, " -t ", t_param, " -cp ", cp_param)
+
+    # cmd <-
+    #   paste0(saving_directory,
+    #          "oslom_undir_win.exe -f OSLOM/dataset.txt -w",
+    #          " -r ", n_runs, " -t ", t_param, " -cp ", cp_param)
+
   } else if(.Platform$OS.type == "unix"){
     stop("To do")
   } else{
@@ -84,7 +102,40 @@ run_oslom <- function(dat, n_runs = 10, t_param = 0.1, cp_param = 0.5,
 
   # Remove all filed in .oslo_files folder
   # unlink("OSLOM/dataset.txt_oslo_files/*", recursive = FALSE)
-  unlink(paste0(.libPaths(),
-                "/Bioregionalization/OSLOM/dataset.txt_oslo_files/*"),
-         recursive = FALSE)
+  # unlink(paste0(.libPaths(),
+  #               "/Bioregionalization/OSLOM/dataset.txt_oslo_files/*"),
+  #        recursive = FALSE)
+  file.remove(paste0(.libPaths(),
+                     "/Bioregionalization/OSLOM/dataset.txt_oslo_files/",
+                     dir(paste0(
+                       .libPaths(),
+                       "/Bioregionalization/OSLOM/dataset.txt_oslo_files/"),
+                       pattern = "net")))
+  file.remove(paste0(.libPaths(),
+                     "/Bioregionalization/OSLOM/dataset.txt_oslo_files/",
+                     dir(paste0(
+                       .libPaths(),
+                       "/Bioregionalization/OSLOM/dataset.txt_oslo_files/"),
+                       pattern = "partitions_level")))
+  file.remove(paste0(.libPaths(),
+                     "/Bioregionalization/OSLOM/dataset.txt_oslo_files/",
+                     dir(paste0(
+                       .libPaths(),
+                       "/Bioregionalization/OSLOM/dataset.txt_oslo_files/"),
+                       pattern = "statistics_level")))
+  file.remove(paste0(.libPaths(),
+                     "/Bioregionalization/OSLOM/dataset.txt_oslo_files/",
+                     dir(paste0(
+                       .libPaths(),
+                       "/Bioregionalization/OSLOM/dataset.txt_oslo_files/"),
+                       pattern = "short_tp")))
+  file.remove(paste0(.libPaths(),
+                     "/Bioregionalization/OSLOM/dataset.txt_oslo_files/",
+                     dir(paste0(
+                       .libPaths(),
+                       "/Bioregionalization/OSLOM/dataset.txt_oslo_files/"),
+                       pattern = "tp")))
+
+  # Reset previous working directory
+  setwd(current_path)
 }
