0fa10f296763948b9c48e263ba511f78b46a3fc8
pierredenelle <pierre.denelle@gmail.com>
2019-10-24 12:14:13 +0200

    Mediterranean dataset added; vignette modified; contingency function modified.

--- R/contingency.R
+++ R/contingency.R
@@ -1,10 +1,41 @@
 
-contingency <- function(dat, sp, site, ab, binary = FALSE){
+contingency <- function(dat, site, sp, ab = NULL, binary = TRUE){
   require(DescTools)
+
+  if(!is.data.frame(dat)){
+    stop("dat must be a data.frame with columns sp and site.")
+  }
+
+  if(!is.character(site)){
+    stop("site must be the column name of dat describing the sites")
+  }
+
+  if(!is.character(sp)){
+    stop("sp must be the column name of dat describing the species.")
+  }
+
+  if(!is.null(ab) & !is.character(ab)){
+    stop("ab must be the column name of dat describing the abundances
+         of species")
+  }
+
+  if(is.null(ab) & binary == FALSE){
+    warning("Without column abundances, contingency table will only get binary
+        values.")
+  }
+
+  if(!is.logical(binary)){
+    stop("binary must be a boolean.")
+  }
+
   # Rename columns
   colnames(dat)[colnames(dat) == sp] <- "sp"
   colnames(dat)[colnames(dat) == site] <- "site"
-  colnames(dat)[colnames(dat) == ab] <- "ab"
+  if(!is.null(ab)){
+    colnames(dat)[colnames(dat) == ab] <- "ab"
+  } else{ # without abundances, species are just present in the sites
+    dat$ab <- 1
+  }
 
   # Create contingency table
   mat <- xtabs(ab ~ site + sp, data = dat)
