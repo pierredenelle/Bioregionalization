6cee67fb1e066416da816628b92b122a82fa828e
pierredenelle <pierre.denelle@gmail.com>
2019-10-25 16:17:47 +0200

    run_oslom: useless lines removed

--- R/run_oslom.R
+++ R/run_oslom.R
@@ -43,13 +43,6 @@ run_oslom <- function(dat, n_runs = 10, t_param = 0.1, cp_param = 0.5,
          the bioregions identified will be saved.")
   }
 
-  # Move OSLOM to saving_directory
-  # path1 <- paste0(.libPaths(), "/Bioregionalization/OSLOM/")
-  # current_files <- list.files(path1, full.names = TRUE)
-  # path2 <- saving_directory
-  # file.copy(from = current_files, to = path2, recursive = TRUE,
-  #           copy.mode = TRUE)
-
   # Save input dataset as a .txt file into OSLOM folder
   write.table(dat,
               # paste0(saving_directory, "dataset.txt"),
@@ -62,19 +55,10 @@ run_oslom <- function(dat, n_runs = 10, t_param = 0.1, cp_param = 0.5,
 
   # Set up the command with required parameters
   if(.Platform$OS.type == "windows"){
-    # cmd <- paste0(
-    #   "D:/PIERRE_DENELLE/CarHab/Bioregionalization/OSLOM/oslom_undir_win.exe -f OSLOM/dataset.txt -w",
-    #   " -r ", n_runs, " -t ", t_param, " -cp ", cp_param)
     cmd <-
       paste0(.libPaths(),
              "/Bioregionalization/OSLOM/oslom_undir_win.exe -f OSLOM/dataset.txt -w",
              " -r ", n_runs, " -t ", t_param, " -cp ", cp_param)
-
-    # cmd <-
-    #   paste0(saving_directory,
-    #          "oslom_undir_win.exe -f OSLOM/dataset.txt -w",
-    #          " -r ", n_runs, " -t ", t_param, " -cp ", cp_param)
-
   } else if(.Platform$OS.type == "unix"){
     stop("To do")
   } else{
@@ -88,26 +72,16 @@ run_oslom <- function(dat, n_runs = 10, t_param = 0.1, cp_param = 0.5,
   tp_res <- readLines(
     paste0(.libPaths(),
            "/Bioregionalization/OSLOM/dataset.txt_oslo_files/tp"))
-  # return("OSLOM/dataset.txt_oslo_files/tp")
 
   # Saving .tp file into chosen saving_directory
   # version = 2: files readable for R versions from 1.4.0 to 3.5.0
-  # save(tp_res, file = paste0(saving_directory, "/tp"), version = 2)
-  # write.table(tp_res, file = paste0(saving_directory, "/tp"),
-  #             row.names = FALSE, col.names = FALSE)
   saveRDS(tp_res, file = paste0(saving_directory, "/tp.rds"), version = 2)
 
   # Remove .oslo_files created and the dataset
-  # file.remove("OSLOM/dataset.txt")
-  # file.remove("OSLOM/time_seed.dat")
   file.remove(paste0(.libPaths(), "/Bioregionalization/OSLOM/dataset.txt"))
   file.remove(paste0(.libPaths(), "/Bioregionalization/OSLOM/time_seed.dat"))
 
   # Remove all filed in .oslo_files folder
-  # unlink("OSLOM/dataset.txt_oslo_files/*", recursive = FALSE)
-  # unlink(paste0(.libPaths(),
-  #               "/Bioregionalization/OSLOM/dataset.txt_oslo_files/*"),
-  #        recursive = FALSE)
   file.remove(paste0(.libPaths(),
                      "/Bioregionalization/OSLOM/dataset.txt_oslo_files/",
                      dir(paste0(
