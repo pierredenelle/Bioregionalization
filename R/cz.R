b59449b4bd189720abd8d8a41cb012c7f51ed52f
pierredenelle <pierre.denelle@gmail.com>
2020-01-29 21:19:48 +0100

    cz function modified: link_dat directly computed inside; man files of cz and zscores changed

--- R/cz.R
+++ R/cz.R
@@ -1,59 +1,50 @@
 
-##-----------------------------------------------------------------------------
-# Function to compute Cz coefficients (Guimera et Amaral 2005)
-# Needs two data frames as inputs:
-
-# link_dat is a data.frame of the links between nodes containing four columns:
-# site, sp, mod_sp, mod_site which stand for the first node category, the
-# second node category, module of the first node and module of the second one
-
-# dat contains three columns: node, mod and cat which respectively stand for
-# the name of the node, its module and its bipartite category
-
-# column of abundance can be added to weight the calculation
-# (not implemented yet)
-
-# The function returns a list containing two data.frame which correspond to
-# dat data.frame with extra-columns Cz, z being only standardized per species
-# in the second data.frame of the list
-
-##-----------------------------------------------------------------------------
-
-cz <- function(link_dat, dat, ab = NULL){
-
-  if(!is.data.frame(link_dat)){
-    stop("link_dat must be a data.frame with four columns containing the links
-    between the two categories of nodes and their respective modules.")
-  }
-
-  if(ncol(link_dat) != 4){
-    stop("link_dat must be a data.frame with four columns containing the links
-    between the two categories of nodes and their respective modules.")
-  }
-
-  if(sort(colnames(link_dat)) != c("mod_site", "mod_sp", "site", "sp")){
-    stop("Columns of dat must be named mod_site, mod_sp, site and sp.")
-  }
+cz <- function(dat, sp_col, site_col, bip, ab = NULL){
 
   if(!is.data.frame(dat)){
-    stop("dat must be a data.frame with three columns containing the nodes,
+    stop("Input must be a data.frame with each row indicating the presence
+         of a species in a given site. The associated bioregion must be present
+         for each site.")
+  }
+
+  if(!is.character(sp_col)){
+    stop("sp_col must be a character string corresponding to the column
+    with species.")
+  }
+
+  if(!is.character(site_col)){
+    stop("site_col must be a character string corresponding to the column
+    with sites")
+  }
+
+  if(!is.data.frame(bip)){
+    stop("bip must be a data.frame with three columns containing the nodes,
          their modules and their bipartite category.")
   }
 
-  if(ncol(dat) != 3){
-    stop("dat must be a data.frame with three columns containing the nodes,
+  if(ncol(bip) != 3){
+    stop("bip must be a data.frame with three columns containing the nodes,
          their modules and their bipartite category.")
   }
 
-  if(sort(colnames(dat)) != c("cat", "mod", "node")){
-    stop("Columns of dat must be named cat, mod and node.")
+  if(sort(colnames(bip)) != c("cat", "mod", "node")){
+    stop("Columns of bip must be named cat, mod and node.")
   }
 
   require(dplyr)
 
+  # Construct table of links
+  link_dat <- dat[, c("site", "sp")] %>%
+    left_join(bip[which(bip$cat == "site"), c("node", "mod")],
+              by = c("site" = "node")) %>%
+    rename(mod_site = mod) %>%
+    left_join(bip[which(bip$cat == "sp"), c("node", "mod")],
+              by = c("sp" = "node")) %>%
+    rename(mod_sp = mod)
+
   # Compute C
   C_site <- c()
-  dat_site <- dat[which(dat$cat == "site"), ]
+  dat_site <- bip[which(bip$cat == "site"), ]
   for(i in 1:nrow(dat_site)){
     tmp2 <- table(link_dat[which(link_dat$site == dat_site[i, "node"]),
                            "mod_sp"])
@@ -62,7 +53,7 @@ cz <- function(link_dat, dat, ab = NULL){
                       1 - sum((tmp2/sum(tmp2))^2), "site"))
   }
   C_sp <- c()
-  dat_sp <- dat[which(dat$cat == "sp"), ]
+  dat_sp <- bip[which(bip$cat == "sp"), ]
   for(i in 1:nrow(dat_sp)){
     tmp2 <- table(link_dat[which(link_dat$sp == dat_sp[i, "node"]), "mod_site"])
     C_sp <- rbind(C_sp,
@@ -73,37 +64,37 @@ cz <- function(link_dat, dat, ab = NULL){
   C_dat <- as.data.frame(C_dat)
   colnames(C_dat) <- c("node", "C", "cat")
 
-  dat <- left_join(dat, C_dat, by = c("node",  "cat"))
-  dat$C <- as.numeric(as.character(dat$C))
+  bip <- left_join(bip, C_dat, by = c("node",  "cat"))
+  bip$C <- as.numeric(as.character(bip$C))
 
   # Compute z
-  dat$n_link_mod <- NA
-  for(i in 1:nrow(dat)){
-    if(dat[i, "cat"] == "site"){
-      tmp2 <- link_dat[which(link_dat$site == dat[i, "node"]), ]
-      dat[i, "n_link_mod"] <- nrow(tmp2[which(tmp2$mod_site == tmp2$mod_sp), ])
+  bip$n_link_mod <- NA
+  for(i in 1:nrow(bip)){
+    if(bip[i, "cat"] == "site"){
+      tmp2 <- link_dat[which(link_dat$site == bip[i, "node"]), ]
+      bip[i, "n_link_mod"] <- nrow(tmp2[which(tmp2$mod_site == tmp2$mod_sp), ])
     } else{
-      tmp2 <- link_dat[which(link_dat$sp == dat[i, "node"]), ]
-      dat[i, "n_link_mod"] <- nrow(tmp2[which(tmp2$mod_site == tmp2$mod_sp), ])
+      tmp2 <- link_dat[which(link_dat$sp == bip[i, "node"]), ]
+      bip[i, "n_link_mod"] <- nrow(tmp2[which(tmp2$mod_site == tmp2$mod_sp), ])
     }
   }
 
-  dat$mod <- as.character(dat$mod)
+  bip$mod <- as.character(bip$mod)
 
-  mean_link_mod <- tapply(dat$n_link_mod, dat$mod, mean)
+  mean_link_mod <- tapply(bip$n_link_mod, bip$mod, mean)
   mean_link_mod <- data.frame("mod" = names(mean_link_mod),
                               "mean_link_mod" = as.numeric(mean_link_mod))
-  dat <- left_join(dat, mean_link_mod, by = "mod")
+  bip <- left_join(bip, mean_link_mod, by = "mod")
 
-  sd_link_mod <- tapply(dat$n_link_mod, dat$mod, sd)
+  sd_link_mod <- tapply(bip$n_link_mod, bip$mod, sd)
   sd_link_mod <- data.frame("mod" = names(sd_link_mod),
                             "sd_link_mod" = as.numeric(sd_link_mod))
-  dat <- left_join(dat, sd_link_mod, by = "mod")
+  bip <- left_join(bip, sd_link_mod, by = "mod")
 
-  dat$z <- (dat$n_link_mod - dat$mean_link_mod) / dat$sd_link_mod
+  bip$z <- (bip$n_link_mod - bip$mean_link_mod) / bip$sd_link_mod
 
   # Z with standardisation only for species
-  dat_sp <- dat[which(dat$cat == "sp"), ]
+  dat_sp <- bip[which(bip$cat == "sp"), ]
   mean_link_mod_sp <- tapply(dat_sp$n_link_mod, dat_sp$mod, mean)
   mean_link_mod_sp <- data.frame("mod" = names(mean_link_mod_sp),
                                  "mean_link_mod_sp" = as.numeric(mean_link_mod_sp))
@@ -115,6 +106,6 @@ cz <- function(link_dat, dat, ab = NULL){
   dat_sp <- left_join(dat_sp, sd_link_mod_sp, by = "mod")
   dat_sp$z <- (dat_sp$n_link_mod - dat_sp$mean_link_mod_sp) / dat_sp$sd_link_mod_sp
 
-  return(list(dat, dat_sp))
+  return(list(bip, dat_sp))
 
 }
